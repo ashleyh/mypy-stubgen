e447dd717354f3d8d5e3d7e3a347c7fdc3779f11
aph <a.hewson@gmail.com>
2013-01-28 23:16:27 +0000

    allow python version to be specified (closes #8)

--- lsmod.py
+++ lsmod.py
@@ -1,9 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import argparse
 
 
 def main():
-    url = 'http://docs.python.org/3/py-modindex.html'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', default='3.2',
+            help='which Python version to list modules for')
+
+    args = parser.parse_args()
+
+    url = 'http://docs.python.org/{}/py-modindex.html'.format(args.version)
     with urlopen(url) as f:
         soup = BeautifulSoup(f.read())
         for a in soup.select('.modindextable a'):
